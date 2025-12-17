754e305f0f04e337775873ae56d2c15950d03edf
Jordan King <jking@hanks.local>
2025-12-17 09:18:09 -0500

    revert

--- extensions/bundle-discount-functionality/src/main.rs
+++ extensions/bundle-discount-functionality/src/main.rs
@@ -1,4 +1,5 @@
 use shopify_function::prelude::*;
+use std::process;
 
 pub mod cart_delivery_options_discounts_generate_run;
 pub mod cart_lines_discounts_generate_run;
@@ -10,4 +11,9 @@ pub mod schema {
 
     #[query("src/cart_delivery_options_discounts_generate_run.graphql")]
     pub mod cart_delivery_options_discounts_generate_run {}
+}
+
+fn main() {
+    log!("Please invoke a named export.");
+    process::abort();
 }
