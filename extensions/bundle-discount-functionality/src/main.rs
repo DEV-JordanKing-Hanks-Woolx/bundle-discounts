2c71f1f94277a15dee6455938fd7e5883321528a
Jordan King <jking@hanks.local>
2025-12-17 14:26:46 -0500

    drastic steps to fix error

--- extensions/bundle-discount-functionality/src/main.rs
+++ extensions/bundle-discount-functionality/src/main.rs
@@ -1,13 +1,42 @@
 use shopify_function::prelude::*;
-pub mod cart_lines_discounts_generate_run;
+use shopify_function::Result;
 
-#[typegen("schema.graphql")]
-pub mod schema {
-    #[query("src/cart_lines_discounts_generate_run.graphql")]
-    pub mod cart_lines_discounts_generate_run {}
-}
+mod cart_lines_discounts_generate_run;
+use cart_lines_discounts_generate_run::run;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    log!("Function modules loaded");
-    Ok(())
+#[shopify_function_target(query_path = "src/cart_lines_discounts_generate_run.graphql", schema_path = "schema.graphql")]
+fn run(input: input::ResponseData) -> Result<output::FunctionRunResult> {
+    // Your discount logic here
+    let mut operations = vec![];
+    
+    // Example: Add a product discount
+    for line in input.cart.lines {
+        operations.push(output::Operation {
+            product_discounts_add: Some(output::ProductDiscountsAdd {
+                candidates: vec![output::ProductDiscount {
+                    message: Some("20% OFF PRODUCT".to_string()),
+                    targets: vec![output::ProductDiscountTarget {
+                        cart_line: Some(output::CartLineTarget {
+                            id: line.id,
+                            quantity: None,
+                        }),
+                    }],
+                    value: output::ProductDiscountValue {
+                        percentage: Some(output::Percentage {
+                            value: 20.0,
+                        }),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                }],
+                selection_strategy: output::SelectionStrategy::First,
+            }),
+            ..Default::default()
+        });
+    }
+    
+    Ok(output::FunctionRunResult {
+        operations,
+        ..Default::default()
+    })
 }
