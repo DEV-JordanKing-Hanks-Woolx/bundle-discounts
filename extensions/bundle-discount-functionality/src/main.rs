4cdb63cb9fc520d378b2020a666e4e37f8baf6c2
Jordan King <jking@hanks.local>
2025-12-17 08:52:11 -0500

    adding space

--- extensions/bundle-discount-functionality/src/main.rs
+++ extensions/bundle-discount-functionality/src/main.rs
@@ -4,6 +4,7 @@ pub mod cart_lines_discounts_generate_run;
 
 #[typegen("schema.graphql")]
 pub mod schema {
+    
     #[query("src/cart_lines_discounts_generate_run.graphql")]
     pub mod cart_lines_discounts_generate_run {}
 }
