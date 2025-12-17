6f7cc5fe465820b904225dfdd01289498bc0c721
Jordan King <jking@hanks.local>
2025-12-17 09:23:41 -0500

    commenting out the uneeded function

--- extensions/bundle-discount-functionality/src/main.rs
+++ extensions/bundle-discount-functionality/src/main.rs
@@ -8,8 +8,8 @@ pub mod schema {
     #[query("src/cart_lines_discounts_generate_run.graphql")]
     pub mod cart_lines_discounts_generate_run {}
 
-    #[query("src/cart_delivery_options_discounts_generate_run.graphql")]
-    pub mod cart_delivery_options_discounts_generate_run {}
+   /* #[query("src/cart_delivery_options_discounts_generate_run.graphql")]
+    pub mod cart_delivery_options_discounts_generate_run {}*/
 }
 
 fn main() -> Result<()> {
