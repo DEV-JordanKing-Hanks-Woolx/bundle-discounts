88ed039ab8496e8eb6e00c4b7692d3d5f5dd3be1
Jordan King <jking@hanks.local>
2025-12-17 09:14:44 -0500

    AI tweak

--- extensions/bundle-discount-functionality/src/main.rs
+++ extensions/bundle-discount-functionality/src/main.rs
@@ -1,5 +1,4 @@
 use shopify_function::prelude::*;
-use std::process;
 
 pub mod cart_delivery_options_discounts_generate_run;
 pub mod cart_lines_discounts_generate_run;
@@ -11,9 +10,4 @@ pub mod schema {
 
     #[query("src/cart_delivery_options_discounts_generate_run.graphql")]
     pub mod cart_delivery_options_discounts_generate_run {}
-}
-
-fn main() {
-    log!("Please invoke a named export.");
-    process::abort();
 }
